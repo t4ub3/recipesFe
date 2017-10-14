bd58900b9d3db7d4d00fa94755e858c619cf55b9
Jonas <jn.jonasneumann@googlemail.com>
2017-10-14 18:09:43 +0200

    added bootstrap & main-nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-nav/main-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recipes-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.less']
+})
+export class MainNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
